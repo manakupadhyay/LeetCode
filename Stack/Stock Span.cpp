9a3c8ead5d09f789e7579caf914467436a94e6f7
Manak Upadhyay <manu742000@gmail.com>
2020-06-22 10:41:09 +0530

    Added Stock Span.cpp

---
+++ Stack/Stock Span.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+void calculate(int arr[], int size)
+{
+    stack<int> stk;
+    int span = 1;
+    cout << span << " ";
+    stk.push(0);
+    for(int i=1;i<size;i++)
+    {
+        while(!stk.empty() && arr[stk.top()] <= arr[i])
+            stk.pop();
+        if(stk.empty())
+            span = i+1;
+        else
+            span = i - stk.top();
+        cout << span << " ";
+        stk.push(i);
+    }
+    cout << endl;
+}
+int main() 
+{
+    int size;
+    cin >> size;
+    int arr[size];
+    for(int i = 0; i < size; i++)
+        cin >> arr[i];
+    calculate(arr,size);
+    return 0;
+}
