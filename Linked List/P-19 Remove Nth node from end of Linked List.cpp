fadce61c2fcca535efbfe488f6bae8915022bff9
Manak Upadhyay <manu742000@gmail.com>
2022-09-26 21:48:07 +0530

    Linked list questions

--- Linked List/P-19 Remove Nth node from end of Linked List.cpp
+++ Linked List/P-19 Remove Nth node from end of Linked List.cpp
@@ -1,5 +1,6 @@
 /*
-Given a linked list, remove the n-th node from the end of list and return its head.
+Given a linked list, remove the n-th node from the end of list and return its
+head.
 
 Example:
 
@@ -8,30 +9,72 @@ Given linked list: 1->2->3->4->5, and n = 2.
 After removing the second node from the end, the linked list becomes 1->2->3->5.
 */
 
-ListNode *removeNthFromEnd(ListNode *head, int n)
-{
-    ListNode *fast = head;
-    ListNode *slow = head;
-    for (int i = 0; i < n; i++)
-        fast = fast->next;
-    if (fast == NULL)
-    {
-        if (slow->next == NULL)
-        {
-            head = NULL;
-            return head;
-        }
-        else
-        {
-            head = head->next;
-            return head;
-        }
+/* Naive Approach */
+/*
+ ListNode* removeNthFromEnd(ListNode* head, int n) {
+    int length = 0;
+    ListNode *temp = head;
+    while(temp){
+        length++;
+        temp = temp->next;
     }
-    while (fast->next != NULL)
-    {
-        fast = fast->next;
-        slow = slow->next;
+    int k = length - n;
+    temp = head;
+    if(k == 0){
+        ListNode* temp2 = head;
+        head= head->next;
+        delete temp2;
+        return head;
     }
-    slow->next = slow->next->next;
+    for(int i = 1;i<k;i++){
+        temp = temp->next;
+    }
+    ListNode *nodeToRemove = temp->next;
+    ListNode *nextNode = nodeToRemove->next;
+    temp->next = nextNode;
+    delete nodeToRemove;
     return head;
+
+    }
+*/
+
+/* My Code */
+ListNode *removeNthFromEnd(ListNode *head, int n) {
+  ListNode *fast = head;
+  ListNode *slow = head;
+  for (int i = 0; i < n; i++)
+    fast = fast->next;
+  if (fast == NULL) {
+    if (slow->next == NULL) {
+      head = NULL;
+      return head;
+    } else {
+      head = head->next;
+      return head;
+    }
+  }
+  while (fast->next != NULL) {
+    fast = fast->next;
+    slow = slow->next;
+  }
+  slow->next = slow->next->next;
+  return head;
+}
+
+/* Two pointer */
+ListNode *removeNthFromEnd(ListNode *head, int n) {
+  ListNode *slow = head;
+  ListNode *fast = head;
+  for (int i = 0; i < n; i++) {
+    fast = fast->next;
+  }
+  if (!fast) {
+    return head->next;
+  }
+  while (fast->next) {
+    slow = slow->next;
+    fast = fast->next;
+  }
+  slow->next = slow->next->next;
+  return head;
 }
--- Linked List/P-876 Middle of linked list.cpp
+++ Linked List/P-876 Middle of linked list.cpp
@@ -1,27 +1,39 @@
 /*
-Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+Given a non-empty, singly linked list with head node head, return a middle node
+of linked list.
 
 If there are two middle nodes, return the second middle node.
-
- 
 
 Example 1:
 
 Input: [1,2,3,4,5]
 Output: Node 3 from this list (Serialization: [3,4,5])
-The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
-Note that we returned a ListNode object ans, such that:
-ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
+The returned node has value 3.  (The judge's serialization of this node is
+[3,4,5]). Note that we returned a ListNode object ans, such that: ans.val = 3,
+ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
 */
 
-ListNode *middleNode(ListNode *head)
-{
-    ListNode *slow = head;
-    ListNode *fast = head;
-    while (fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-    return slow;
+/* Naive approach */
+/*
+int length = 0;
+ListNode *temp = head;
+while (temp) {
+  length++;
+  temp = temp->next;
+}
+temp = head;
+for (int i = 0; i < length / 2; i++) {
+  temp = temp->next;
+}
+return temp;
+*/
+
+ListNode *middleNode(ListNode *head) {
+  ListNode *slow = head;
+  ListNode *fast = head;
+  while (fast && fast->next) {
+    slow = slow->next;
+    fast = fast->next->next;
+  }
+  return slow;
 }
